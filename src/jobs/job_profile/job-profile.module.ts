9ba0293e4d1495b5750ea4a248790cc156512efc
Basel Hijazi <flutter@royalclass.group>
2025-03-21 09:27:23 +0400

    Refactoring the modules.
    Refactoring the modules.

--- src/jobs/job_profile/job-profile.module.ts
+++ src/jobs/job_profile/job-profile.module.ts
@@ -1,11 +1,47 @@
 import { Module } from '@nestjs/common';
-import { ResumesModule } from './modules/resumes.module';
-import { QualificationsModule } from './modules/qualifications.module';
+import { QualificationsEntity } from './entities/qualification.entity';
+import { DegreesEntity } from 'src/core/entities/degrees.entity';
+import { SchoolsEntity } from 'src/core/entities/schools.entity';
+import { CityEntity } from 'src/core/entities/cities.entity';
+import { CountryEntity } from 'src/core/entities/countries.entity';
+import { Users } from 'src/core/entities/users.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { QualificationsService } from './services/qualifications.service';
+import { QualificationsRepository } from './repositories/qualifications.repository';
+import { QualificationsController } from './controllers/qualifications.controller';
+import { WasabiModule } from 'src/common/helpers/modules/wasabi.module';
+import { ResumesController } from './controllers/resumes.controller';
+import { ResumesService } from './services/resumes.service';
+import { ResumesRepository } from './repositories/resumes.repository';
+import { ResumeEntity } from './entities/resume.entity';
+import { SkillsService } from './services/skills.service';
+import { SkillsController } from './controllers/skills.controller';
 
 @Module({
     imports: [
-        QualificationsModule,
-        ResumesModule,
+        TypeOrmModule.forFeature([
+            Users,
+            CityEntity,
+            CountryEntity,
+            SchoolsEntity,
+            DegreesEntity,
+            ResumeEntity,
+            QualificationsEntity,
+        ]),
+        WasabiModule,
+
     ],
+    controllers: [
+        QualificationsController,
+        ResumesController,
+        SkillsController
+    ],
+    providers: [
+        ResumesRepository,
+        ResumesService,
+        QualificationsRepository,
+        QualificationsService,
+        SkillsService
+    ]
 })
 export class JobProfileModule { }
--- src/jobs/job_profile/modules/skills.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SkillsController } from '../controllers/skills.controller';
-import { SkillsService } from '../services/skills.service';
-
-@Module({
-  controllers: [SkillsController],
-  providers: [SkillsService],
-})
-export class SkillsModule { }
